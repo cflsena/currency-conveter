9c82bae5b742e775030e8a441bb8cf73bb0bb543
Cleiton Sena <cflsena2@gmail.com>
2025-04-14 23:25:15 -0300

    feat: added customized exceptions

---
+++ src/main/kotlin/br/com/jaya/tech/domain/common/exception/exceptions.kt
@@ -0,0 +1,20 @@
+package br.com.jaya.tech.domain.common.exception
+
+import br.com.jaya.tech.shared.exception.BaseException
+import br.com.jaya.tech.shared.exception.Error
+
+class DomainException private constructor(error: Error) : BaseException(error) {
+    companion object {
+        fun with(message: String): DomainException {
+            return DomainException(Error.with(message))
+        }
+    }
+}
+
+class NotFoundException private constructor(error: Error) : BaseException(error) {
+    companion object {
+        fun with(message: String): NotFoundException {
+            return NotFoundException(Error.with(message))
+        }
+    }
+}
---
+++ src/main/kotlin/br/com/jaya/tech/shared/exception/BaseException.kt
@@ -0,0 +1,14 @@
+package br.com.jaya.tech.shared.exception
+
+import java.io.Serializable
+
+data class Error(val message: String) : Serializable {
+    companion object {
+        fun with(message: String): Error {
+            return Error(message)
+        }
+    }
+}
+
+open class BaseException (private val error: Error) : RuntimeException(error.message, null, true, false) {
+}
