fde17c3919da8235b315a5130b52e1d7091e7d70
Cleiton Sena <cflsena2@gmail.com>
2025-04-16 00:26:27 -0300

    feat: base app

---
+++ src/test/kotlin/br/com/jaya/tech/CurrencyConverterApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jaya.tech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyConverterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
