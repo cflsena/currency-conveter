42a7ed23e00ce4fac32f0f6739f65c2ecf45a230
Cleiton Sena <cflsena2@gmail.com>
2025-04-16 09:52:43 -0300

    feat: added update user account use case

---
+++ src/main/kotlin/br/com/jaya/tech/application/user/update/UpdateUserAccountUseCase.kt
@@ -0,0 +1,6 @@
+package br.com.jaya.tech.application.user.update
+
+import br.com.jaya.tech.application.UseCaseNoOutput
+
+interface UpdateUserAccountUseCase : UseCaseNoOutput<UpdateUserAccountInput> {
+}
--- src/main/kotlin/br/com/jaya/tech/domain/user/UserRepository.kt
+++ src/main/kotlin/br/com/jaya/tech/domain/user/UserRepository.kt
@@ -3,4 +3,5 @@ package br.com.jaya.tech.domain.user
 interface UserRepository {
     fun save(user: User) : User
     fun existsByEmail(email: String): Boolean
+    fun findById(id: String) : User?
 }
