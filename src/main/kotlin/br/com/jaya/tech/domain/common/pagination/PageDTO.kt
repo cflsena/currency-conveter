3d00b36667a9f8a3e3f2c0e61f001b74f478426e
Cleiton Sena <cflsena2@gmail.com>
2025-04-17 00:40:12 -0300

    feat: added pagination component

---
+++ src/main/kotlin/br/com/jaya/tech/domain/common/pagination/PageDTO.kt
@@ -0,0 +1,11 @@
+package br.com.jaya.tech.domain.common.pagination
+
+class PageDTO<T>(
+    val pageNumber: Int,
+    val pageSize: Int,
+    val numberOfElements: Int,
+    val totalPages: Int,
+    val totalElements: Int,
+    val items: List<T>
+) {
+}
