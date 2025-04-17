b4825addbc3bbfbc23689c7002975298bf90fa48
Cleiton Sena <cflsena2@gmail.com>
2025-04-17 14:06:54 -0300

    test: added integration test for user repository

---
+++ src/test/resources/sql/user/insert_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO JAYA_TECH.tb_user (id, given_name, family_name, email)
+values('85fc5f36-fe55-4d77-816d-8f6edfd395d5', 'John', 'Doe', 'test@test.com');
+
+INSERT INTO JAYA_TECH.tb_user (id, given_name, family_name, email)
+values('a8ac2072-bb41-458f-8a75-aaefbd24a42b', 'John', 'Doe', 'dan@targaryen.com');
